a0795de13068fc0cc72bbd8e5c74f32333fb5210
Geert Keularts <geertkeularts@gmail.com>
2016-06-07 11:59:54 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,26 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :dutchies, only: [:index, :show] do
+    resources :requests, only: [:new, :create]
+  end
+
+  namespace :dutchies do
+    resources :requests, only: [:index] do
+      member do
+        patch :accept
+        patch :refuse
+      end
+    end
+  end
+
+
+  namespace :refugees do
+    resources :requests, only: [:index]
+  end 
+
+
+
   root to: 'pages#home'
   
 end
