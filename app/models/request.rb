dc6fa41822fd8a1661b1a2cc0386ae34f3283f61
Merel <merel.steenbrink@gmail.com>
2016-06-07 17:49:09 +0200

    Add seedfile

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,4 @@
 class Request < ActiveRecord::Base
   belongs_to :refugee, class_name: :user, foreign_key: :refugee_id
-  belongs_to :dutchie, class_name: :user, foreign_key: :dutchie_id
+  belongs_to :dutchy, class_name: :user, foreign_key: :dutchy_id
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160607102245) do
+ActiveRecord::Schema.define(version: 20160607143718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20160607102245) do
     t.text     "message"
     t.string   "status"
     t.integer  "refugee_id"
-    t.integer  "dutchie_id"
+    t.integer  "dutchy_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
