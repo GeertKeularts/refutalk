919aad378606246d6a9cddd86080c5e119cdf4e4
Aafke Smal <aafke.smal@gmail.com>
2016-06-09 15:50:24 +0200

    Add validation for request

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,9 @@
 class Request < ActiveRecord::Base
   belongs_to :refugee, class_name: 'User', foreign_key: :refugee_id
   belongs_to :dutchy, class_name: 'User', foreign_key: :dutchy_id
+
+  validates :message, presence: true
+  validates :status, :inclusion => {in: ["pending", "declined", "accepted"]}
+  validates :refugee_id, :dutchy_id, presence: true
+  validates :dutchy_id, uniqueness: {scope: :refugee_id, message: "You've already send a request"}
 end
